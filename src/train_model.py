b790e784779519794919d0c5733e6347b6b8a89f
Varaxy <raxysteve@gmail.com>
2025-07-06 13:17:40 -0700

    Max_iter, liblinear

--- src/train_model.py
+++ src/train_model.py
@@ -30,7 +30,7 @@ y = df['Survived']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Train
-model = LogisticRegression(solver='liblinear')
+model = LogisticRegression(max_iter=20000, solver='liblinear')
 model.fit(X_train, y_train)
 
 # Predict
